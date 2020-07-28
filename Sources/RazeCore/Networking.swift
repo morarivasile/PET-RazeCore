107b0b24228130669f7b206fb59526a415853505
Vasile Morari <>
2020-07-28 15:03:12 +0300

    Added first networkingAPI for retrieving network data

--- Sources/RazeCore/Networking.swift
+++ Sources/RazeCore/Networking.swift
@@ -8,8 +8,28 @@
 import Foundation
 
 extension RazeCore {
-    public class Netwlrking {
+    public class Networking {
         
+        /// Responsible for handling all networking calls
+        /// - Warning: Must create before using any public APIs
+        public class Manager {
+            public init() {}
+            
+            private let session = URLSession.shared
+            
+            public func loadData(from url: URL, completionHandler: @escaping (NetworkResult<Data>) -> Void) {
+                let task = session.dataTask(with: url) { data, resopnse, error in
+                    let result = data.map(NetworkResult<Data>.success) ?? .failure(error)
+                    completionHandler(result)
+                }
+                task.resume()
+            }
+        }
+        
+        public enum NetworkResult<Value> {
+            case success(Value)
+            case failure(Error?)
+        }
     }
     
 }
